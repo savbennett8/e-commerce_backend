a9c84d3755d297c9ac9e1dfc799b23681861204d
Savannah Bennett <savvy.bennett8@gmail.com>
2021-04-25 13:50:50 -0600

    associations created for models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,44 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: 'tagged_product',
+    foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: 'tagged_product',
+    foreignKey: 'tag_id'
+});
+
+ProductTag.belongsTo(Tag, {
+    foreignKey: 'tag_id'
+});
+
+ProductTag.belongsTo(Product, {
+    foreignKey: 'product_id'
+});
+
+Product.hasMany(ProductTag, {
+    foreignKey: 'product_id'
+});
+
+Tag.hasMany(ProductTag, {
+    foreignKey: 'tag_id'
+});
 
 module.exports = {
     Product,
